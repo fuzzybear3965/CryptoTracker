8066e9f2891726fc9e12600200387200fc363844
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-03-13 13:28:00 -0400

    refactored and added some padding

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,6 @@
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
 var currencies = ["cad","usd","xbt","dash","eth"];
 var currencyDropdownEl = document.createElement("SELECT");
 var currencyOptions = [];
@@ -52,4 +55,32 @@ function addRow(isInitial) {
 
 function removeRow(evt) {
 this.parentElement.parentElement.removeChild(this.parentElement)
+}
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+   // Create the data table.
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', 'Topping');
+   data.addColumn('number', 'Slices');
+   data.addRows([
+         ['Mushrooms', 3],
+         ['Onions', 1],
+         ['Olives', 1],
+         ['Zucchini', 1],
+         ['Pepperoni', 2]
+   ]);
+
+   // Set chart options
+   var options = {'title':'How Much Pizza I Ate Last Night',
+      'width':600,
+      'height':300};
+
+   // Instantiate and draw our chart, passing in some options.
+   var chart = new google.visualization.PieChart(document.getElementById('chart'));
+   chart.draw(data, options);
 }
